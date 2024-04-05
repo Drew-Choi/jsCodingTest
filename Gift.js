f3434fcf687e099c9ffdedd503e310d5681aeff4
Drew <drc920630@gmail.com>
2024-04-06 00:01:51 +0900

    계속 연구 중

--- Gift.js
+++ Gift.js
@@ -2,18 +2,33 @@ const friends = ["muzi", "ryan", "frodo", "neo"];
 const gifts = [
   "muzi frodo",
   "muzi frodo",
-  // "ryan muzi",
-  // "ryan muzi",
-  // "ryan muzi",
-  // "frodo muzi",
-  // "frodo ryan",
-  // "neo muzi",
+  "ryan muzi",
+  "ryan muzi",
+  "ryan muzi",
+  "frodo muzi",
+  "frodo ryan",
+  "neo muzi",
 ];
 
 const solution = (friends, gifts) => {
-  const arr = friends.reduce((acc, cur) => {}, {});
+  let arr = friends.reduce((acc, friend) => {
+    if (!acc[friend]) {
+      acc[friend] = {};
+    }
 
-  console.log(arr);
+    gifts.forEach((gift) => {
+      const [from, to] = gift.split(" ");
+
+      if (friend === from) {
+        if (!acc[friend][to]) {
+          acc[friend][to] = 0;
+        }
+        acc[friend][to]++;
+      }
+    });
+
+    return acc;
+  }, {});
 };
 
 solution(friends, gifts);
